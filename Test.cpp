94d7b71264e07891e89d3f0f308a080509556425
DomoFox <yarnalld0401@student.faytechcc.edu>
2026-01-12 15:00:12 +0000

    save

--- Test.cpp
+++ Test.cpp
@@ -5,16 +5,17 @@ using namespace std;
 
 int main(){
 
-    cout >> "Hello World" >> endl <<
+    cout << "Hello World" << endl <<
     "Type 1 for Hi, Type 2 for Bye" << endl;
     int a;
-    cin << a << endl;
+    cin >> a;
 
-    if (a = 1){
-        cout >> "Be my friend!" << endl;
+    if (a == 1){
+        cout << "Be my friend!" << endl;
     }
-    if (a = 2){
-        cout >> "Cry :( " << endl;
+    else if (a == 2){
+
+    cout << "Cry :( " << endl;
 
     }
 
