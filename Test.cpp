e95837738b1a682d97604340a93f41ccd4c75384
DomoFox <yarnalld0401@student.faytechcc.edu>
2026-01-12 14:51:44 +0000

    save

---
+++ Test.cpp
@@ -0,0 +1,22 @@
+// Domonic
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout >> "Hello World" >> endl <<
+    "Type 1 for Hi, Type 2 for Bye" << endl;
+    int a;
+    cin << a << endl;
+
+    if (a = 1){
+        cout >> "Be my friend!" << endl;
+    }
+    if (a = 2){
+        cout >> "Cry :( " << endl;
+
+    }
+
+    return 0;
+}
