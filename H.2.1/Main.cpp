e5d394a19a0a3642fb79605da5a6c576d682c066
DomoFox <yarnalld0401@student.faytechcc.edu>
2026-03-15 05:48:20 +0000

    save

--- H.2.1/Main.cpp
+++ H.2.1/Main.cpp
@@ -6,9 +6,38 @@ H.2.1 The standard list */
 
 using namespace std;
 
+void Invintory();
+
 int main (){
 
-    
+    cout << "Welcome to the main menu! You are a player in an RPG game. Would you like to:" <<
+    endl << "Press 1 - Veiw invintory list " << endl << "Press 2 - Search inventory list " << endl <<
+    "Press 3 - Exit " << endl;
+
+    cout << "Inventory: " << endl;
+
+    int mainMenuChoice;
+    cin >> mainMenuChoice;
+
+    if (mainMenuChoice == 1){
+        Invintory;
+    }
+    else if(mainMenuChoice == 2){
+        cout << "Let's search!" << endl;
+    }
+    else if (mainMenuChoice == 3){
+        cout << "Exiting..." << endl;
+    }
+    else{
+        cout << "Uh oh, that was not a choice :,( " << endl;
+    }
 
     return 0;
+}
+
+void Invintory(){
+
+    cout<< "Inventory";
+
+   ;
 }
