63bed135415956aa3b56e52450d353f82d1f4349
DomoFox <yarnalld0401@student.faytechcc.edu>
2026-04-16 20:12:04 +0000

    save

--- H.4.1/hashing_DomonicYarnall.cpp
+++ H.4.1/hashing_DomonicYarnall.cpp
@@ -18,21 +18,45 @@ unsigned long hashPassword(string password) {
 
 int main(){
 
-    string username;
+    string username1;
     string userpass;
+
+    string username2;
+    string userpass2;
+
+    string username3;
+    string userpass3;
 
 
     cout << "Please Enter Your Username: ";
-    cin >>  username;
+    cin >>  username1;
     cout << endl;
     cout << "Please Enter Your Password: " ;
     cin >> userpass;
     cout << endl;
 
+    cout << "Please Enter Your Username: ";
+    cin >>  username2;
+    cout << endl;
+    cout << "Please Enter Your Password: " ;
+    cin >> userpass2;
+    cout << endl;
+
+    cout << "Please Enter Your Username: ";
+    cin >>  username3;
+    cout << endl;
+    cout << "Please Enter Your Password: " ;
+    cin >> userpass3;
+    cout << endl;
+
     unsigned long result = hashPassword(userpass);
+    unsigned long result2 = hashPassword(userpass2);
+    unsigned long result3 = hashPassword(userpass3);
    
 
-    cout << "Your password hash is: " << result << endl;
+        cout << username1 << ": your password hash is: " << result << endl;
+        cout << username2 << ": your password hash is: " << result2 << endl;
+        cout << username3 << ": your password hash is: " << result3 << endl;
 
 
 
